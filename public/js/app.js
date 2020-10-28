bda6d2a2833269c93e5d5aaef9529b4fd4e47bac
jeremiahiro <jeremiahiro@gmail.com>
2020-10-28 20:43:19 +0100

    fix: app.js|

--- public/js/app.js
+++ public/js/app.js
@@ -54473,8 +54473,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\UCC\Documents\Code\Laravel\tronweb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\UCC\Documents\Code\Laravel\tronweb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\UCC\Documents\Code\Laravel\tronweb-vue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\UCC\Documents\Code\Laravel\tronweb-vue\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
